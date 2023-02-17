c05b7b3ad4b3d35ef03631033ad6cbfeac092382
algo-za <116758928+algo-za@users.noreply.github.com>
2023-02-17 15:40:42 +0200

    Update

--- IBKR API Py Scripts/app.py
+++ IBKR API Py Scripts/app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def hello_world():  # put application's code here
     return '<h1>Hello, World!</h1>'
 
+@app.route('/about/')
+def about():
+    return '<h3>This is a Flask web application.</h3>'
+
 
 if __name__ == '__main__':
     app.run()
